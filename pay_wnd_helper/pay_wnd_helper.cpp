42d983f7b65c15d7152e7bc6241f76a2ca9fde99
zengyu <zengyu@yy.com>
2018-07-11 19:55:03 +0800

    [feature][webbrowser基础文件]

---
+++ pay_wnd_helper/pay_wnd_helper.cpp
@@ -0,0 +1,38 @@
+﻿#include "pay_wnd_helper.h"
+#include <atlhost.h>
+#include "WebDialog.h"
+#include "web_extern_dispatch.h"
+#include "custom_control_site.h"
+bool PayWndHelper::InitPayWndSupport() {
+	if (AfxWinInit(GetModuleHandle(NULL), NULL, GetCommandLine(), 0) == FALSE) {
+			return false;
+		}
+
+		if (S_OK != OleInitialize(0) || !AtlAxWinInit())
+			return false;
+	AfxEnableControlContainer(new CCustomOccManager);
+	m_dispatch = new WebExternDispatch;
+	m_init = true;
+	return true;
+}
+
+PayWndHelper::~PayWndHelper() {
+	if (m_dispatch) {
+		delete m_dispatch;
+	}
+}
+
+bool PayWndHelper::ShowPayWnd(const std::string& pay_url) {
+	if (!m_init) {
+		return false;
+	}
+	CWebDialog WebDlg(pay_url.c_str(), "Buy", CWnd::FromHandle(GetActiveWindow()));
+	WebDlg.SetSize(1600, 900);
+	WebDlg.DoModal();
+	return true;
+}
+void PayWndHelper::OpenUrl(const std::string& url) {
+	HINSTANCE result = ::ShellExecute(NULL, "open", url.c_str(), NULL, NULL, SW_SHOW);
+	if ((int)result <= HINSTANCE_ERROR)
+		result = ::ShellExecute(NULL, NULL, "iexplore.exe", url.c_str(), NULL, SW_SHOW);
+}
---
+++ pay_wnd_helper/pay_wnd_helper.h
@@ -0,0 +1,17 @@
+﻿#ifndef PAY_WND_HELPER_H_
+#define PAY_WND_HELPER_H_
+#include "base/singleton.h"
+class WebExternDispatch;
+class CCustomOccManager;
+class PayWndHelper : public yy::Singleton<PayWndHelper> {
+public:
+	bool InitPayWndSupport();
+	bool ShowPayWnd(const std::string& pay_url);
+	void OpenUrl(const std::string& url);
+	WebExternDispatch* GetExternDispatch() { return m_dispatch; }
+	~PayWndHelper();
+private:
+	bool m_init = false;
+	WebExternDispatch* m_dispatch = nullptr;
+};
+#endif // !PAY_WND_HELPER_H_
